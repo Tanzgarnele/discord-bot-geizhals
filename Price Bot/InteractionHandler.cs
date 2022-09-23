f4d64bf467b3db7c4949a4c9ddb5e3bf7205eb99
tobias hansen <Tanzgarnele@outlook.com>
2022-09-24 00:23:41 +0200

    Add project files.

---
+++ Price Bot/InteractionHandler.cs
@@ -0,0 +1,42 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System.Reflection;
+
+namespace Price_Bot
+{
+    public class InteractionHandler
+    {
+        private readonly DiscordSocketClient client;
+        private readonly InteractionService commands;
+        private readonly IServiceProvider services;
+
+        public InteractionHandler(DiscordSocketClient client, InteractionService commands, IServiceProvider services)
+        {
+            this.client = client;
+            this.commands = commands;
+            this.services = services;
+        }
+
+        public async Task InitalizeAsync()
+        {
+            await this.commands.AddModulesAsync(Assembly.GetEntryAssembly(), this.services);
+
+            client.InteractionCreated += HandleInteraction;
+        }
+
+        private async Task HandleInteraction(SocketInteraction arg)
+        {
+            try
+            {
+                var context = new SocketInteractionContext(this.client, arg)
+                    await this.commands.ExecuteCommandAsync(context, this.services);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Price Bot/Program.cs
@@ -0,0 +1,49 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+
+namespace Price_Bot
+{
+    public class Program
+    {
+        public static Task Main() => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+            using IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                services.AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    GatewayIntents = GatewayIntents.AllUnprivileged,
+                    AlwaysDownloadUsers = true
+                }))).Build();
+
+            await RunAsync(host);
+        }
+
+        public async Task RunAsync(IHost host)
+        {
+            using IServiceScope servicescope = host.Services.CreateScope();
+            IServiceProvider provider = servicescope.ServiceProvider;
+
+            DiscordSocketClient client = provider.GetRequiredService<DiscordSocketClient>();
+
+            client.Log += async (LogMessage msg) => { Console.WriteLine(msg.Message); };
+            client.Ready += async () =>
+            {
+                Console.WriteLine("Bot started!");
+            };
+
+            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken", EnvironmentVariableTarget.User));
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+
+    }
+}
