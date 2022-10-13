0d7102bf61e40edf8f4a5e3e6e798c312ca706da
Tanzgarnele <hansentobias98@gmail.com>
2022-10-13 19:06:15 +0200

    Testing without DeferAsync

--- ManfredHorst/Program.cs
+++ ManfredHorst/Program.cs
@@ -9,59 +9,59 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ManfredHorst
 {
-    public class Program
-    {
-        private readonly IConfiguration config;
-        private readonly IServiceProvider services;
+	public class Program
+	{
+		private readonly IConfiguration config;
+		private readonly IServiceProvider services;
 
-        private readonly DiscordSocketConfig socketConfig = new()
-        {
-            GatewayIntents = GatewayIntents.AllUnprivileged,
-            AlwaysDownloadUsers = true,
-            LogLevel = LogSeverity.Info,
-            MessageCacheSize = 50,
-            UseInteractionSnowflakeDate = true,
-        };
+		private readonly DiscordSocketConfig socketConfig = new()
+		{
+			GatewayIntents = GatewayIntents.AllUnprivileged,
+			AlwaysDownloadUsers = true,
+			LogLevel = LogSeverity.Info,
+			MessageCacheSize = 50,
+			UseInteractionSnowflakeDate = true,
+		};
 
-        public Program()
-        {
-            this.config = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
-                .AddYamlFile("config.yml")
-                .Build();
+		public Program()
+		{
+			this.config = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
+				.AddYamlFile("config.yml")
+				.Build();
 
-            this.services = new ServiceCollection()
-                .AddSingleton(this.config)
-                .AddSingleton(this.socketConfig)
-                .AddSingleton<DiscordSocketClient>()
-                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
-                .AddSingleton(x => new TimerService(x.GetRequiredService<DiscordSocketClient>()))
-                .AddSingleton<InteractionHandler>()
-                .AddSingleton<ISqlDataAccess, SqlDataAccess>()
-                .AddSingleton<IProductData, ProductData>()
-                .BuildServiceProvider();
-        }
+			this.services = new ServiceCollection()
+				.AddSingleton(this.config)
+				.AddSingleton(this.socketConfig)
+				.AddSingleton<DiscordSocketClient>()
+				.AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+				.AddSingleton(x => new TimerService(x.GetRequiredService<DiscordSocketClient>()))
+				.AddSingleton<InteractionHandler>()
+				.AddSingleton<ISqlDataAccess, SqlDataAccess>()
+				.AddSingleton<IProductData, ProductData>()
+				.BuildServiceProvider();
+		}
 
-        private static void Main(string[] args)
-            => new Program().RunAsync()
-                .GetAwaiter()
-                .GetResult();
+		private static void Main(string[] args)
+			=> new Program().RunAsync()
+				.GetAwaiter()
+				.GetResult();
 
-        public async Task RunAsync()
-        {
-            DiscordSocketClient client = this.services.GetRequiredService<DiscordSocketClient>();
+		public async Task RunAsync()
+		{
+			DiscordSocketClient client = this.services.GetRequiredService<DiscordSocketClient>();
 
-            client.Log += LogAsync;
+			client.Log += LogAsync;
 
-            await this.services.GetRequiredService<InteractionHandler>().InitalizeAsync();
-            await this.services.GetRequiredService<TimerService>().InitalizeAsync();
+			await this.services.GetRequiredService<InteractionHandler>().InitalizeAsync();
+			await this.services.GetRequiredService<TimerService>().InitalizeAsync();
 
-            await client.LoginAsync(TokenType.Bot, this.config["tokens:discord"]);
-            await client.StartAsync();
+			await client.LoginAsync(TokenType.Bot, this.config["tokens:discord"]);
+			await client.StartAsync();
 
-            await Task.Delay(Timeout.Infinite);
-        }
+			await Task.Delay(Timeout.Infinite);
+		}
 
-        private async Task LogAsync(LogMessage message)
-            => Console.WriteLine(message.ToString());
-    }
+		private async Task LogAsync(LogMessage message)
+			=> Console.WriteLine(message.ToString());
+	}
 }
