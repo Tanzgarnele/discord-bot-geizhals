274dff073883b0dd614f6ddda2f10c756f9b7136
Tanzgarnele <hansentobias98@gmail.com>
2023-01-14 17:18:19 +0100

    Switching to Discord.Addons.Hosting 1

---
+++ ManfredHorst/LongRunningService.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLibrary.Interfaces;
+using DataAccessLibrary.Models;
+using Discord;
+using Discord.Addons.Hosting;
+using Discord.Addons.Hosting.Util;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ManfredHorst;
+
+public class LongRunningService : DiscordClientService
+{
+    private readonly IConfiguration config;
+    private readonly IProductData productData;
+    private readonly IGeizhalsScraper geizhalsScraper;
+
+    public LongRunningService(DiscordSocketClient client, ILogger<DiscordClientService> logger, IConfiguration config, IProductData productData, IGeizhalsScraper geizhalsScraper) : base(client, logger)
+    {
+        this.config = config ?? throw new ArgumentNullException(nameof(config));
+        this.productData = productData ?? throw new ArgumentNullException(nameof(productData));
+        this.geizhalsScraper = geizhalsScraper ?? throw new ArgumentNullException(nameof(geizhalsScraper));
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await Client.WaitForReadyAsync(stoppingToken);
+
+        new Timer(DoWork, !stoppingToken.IsCancellationRequested, TimeSpan.FromSeconds(5), TimeSpan.FromMinutes(20));
+    }
+
+    private async void DoWork(object state)
+    {
+        foreach (Alarm alarm in await productData.GetAlarms())
+        {
+            if (await this.geizhalsScraper.ScrapeGeizhals(alarm))
+            {
+                if (Client.GetChannel(Convert.ToUInt64(this.config["output:live"])) is IMessageChannel chan)
+                {
+                    Logger.LogInformation($"Alarm {alarm.Alias} from {alarm.Mention} deleted {DateTime.Now}");
+                    await chan.SendMessageAsync($"**{alarm.Alias}** below **{alarm.Price}€**\n{alarm.Url}\n {alarm.Mention} Alarm deleted!");
+                    await this.productData.DeleteAlarm(alarm.Alias, alarm.Mention);
+                }
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+        }
+    }
+}
--- ManfredHorst/Program.cs
+++ ManfredHorst/Program.cs
@@ -2,80 +2,51 @@
 using DataAccessLibrary.Scraper;
 using DataAccessLibrary.Sql;
 using Discord;
+using Discord.Addons.Hosting;
 using Discord.Interactions;
 using Discord.WebSocket;
+using ManfredHorst;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Yaml;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace ManfredHorst;
-
-public class Program
-{
-    private readonly IConfiguration config;
-    private readonly DiscordSocketClient client;
-
-    public Program(DiscordSocketClient client, IConfiguration config)
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureDiscordHost((context, config) =>
     {
-        this.client = client ?? throw new ArgumentNullException(nameof(client));
-        this.config = config ?? throw new ArgumentNullException(nameof(config));
-    }
+        config.SocketConfig = new DiscordSocketConfig
+        {
+            LogLevel = LogSeverity.Verbose,
+            AlwaysDownloadUsers = true,
+            MessageCacheSize = 200
+        };
 
-    public static IHostBuilder CreateHostBuilder(string[] args)
+        context.Configuration = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
+            .AddYamlFile("config.yml")
+            .Build();
+
+        config.Token = context.Configuration["tokens:discord"];
+    })
+    .ConfigureServices((hostContext, services) =>
     {
-        return Host.CreateDefaultBuilder(args)
-            .ConfigureServices((hostContext, services) =>
-            {
-                IConfigurationRoot config = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
-                    .AddYamlFile("config.yml")
-                    .Build();
+        IConfigurationRoot config = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
+            .AddYamlFile("config.yml")
+            .Build();
 
-                services.AddSingleton<IConfiguration>(config);
+        services.AddSingleton<IConfiguration>(config);
 
-                services.AddSingleton(new DiscordSocketConfig
-                {
-                    GatewayIntents = GatewayIntents.AllUnprivileged,
-                    AlwaysDownloadUsers = true,
-                    LogLevel = LogSeverity.Info,
-                    MessageCacheSize = 50,
-                });
+        services.AddHostedService<BotStatusService>();
+        services.AddHostedService<LongRunningService>();
+        //services.AddHostedService<TimerService>();
+        //services.AddSingleton<DiscordService>();
+        //services.AddSingleton<TimerService>();
+        //services.AddSingleton<DiscordSocketClient>();
+        //services.AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()));
+        //services.AddSingleton<InteractionHandler>();
+        services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+        services.AddSingleton<IProductData, ProductData>();
+        services.AddSingleton<IGeizhalsScraper, GeizhalsScraper>();
+    }).Build();
 
-                services.AddHostedService<TimerService>();
-                services.AddSingleton<TimerService>();
-                services.AddSingleton<Program>();
-                services.AddSingleton<DiscordSocketClient>();
-                services.AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()));
-                services.AddSingleton<InteractionHandler>();
-                services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
-                services.AddSingleton<IProductData, ProductData>();
-                services.AddSingleton<IGeizhalsScraper, GeizhalsScraper>();
-
-            });
-    }
-
-    private static void Main(string[] args)
-    {
-        using IHost host = CreateHostBuilder(args).Build();
-        host.Start();
-        host.Services.GetRequiredService<InteractionHandler>().InitalizeAsync().GetAwaiter().GetResult();
-        host.Services.GetRequiredService<Program>().RunAsync().GetAwaiter().GetResult();
-    }
-
-
-    public async Task RunAsync()
-    {
-        client.Log += LogAsync;
-        Console.WriteLine($"{DateTime.Now} SQL Server is online! Starting the Bot now.");
-
-        await client.LoginAsync(TokenType.Bot, this.config["tokens:discord"]);
-        await client.StartAsync();
-
-        await Task.Delay(Timeout.Infinite);
-    }
-
-    private async Task LogAsync(LogMessage message)
-    {
-        Console.WriteLine(message.ToString());
-    }
-}
+//host.Services.GetRequiredService<InteractionHandler>().InitalizeAsync().GetAwaiter().GetResult();
+await host.RunAsync();
